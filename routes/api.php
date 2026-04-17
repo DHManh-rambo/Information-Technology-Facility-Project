e9f21abb4bff752a5c2c0f7f46a3e1027fb1da8a
DHManh-rambo <duonghmanh123@gmail.com>
2026-04-17 16:39:43 +0700

    test api

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\DB;
 
-Route::get('/khachhang', function () {
-    $data = DB::table('khach_hang')->get();
+Route::get('/sanpham', function () {
+    $data = DB::table('san_pham')->get();
+
+    return response()->json($data);
+});
+Route::get('/hoadon', function () {
+    $data = DB::table('hoa_don')->get();
 
     return response()->json($data);
 });
