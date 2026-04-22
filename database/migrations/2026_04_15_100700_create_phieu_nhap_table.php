5445ba48f58de30d7b7b2473f91c22b46565f2ee
DHManh-rambo <duonghmanh123@gmail.com>
2026-04-22 18:03:09 +0700

    fix lại database

--- database/migrations/2026_04_15_100700_create_phieu_nhap_table.php
+++ database/migrations/2026_04_15_100700_create_phieu_nhap_table.php
@@ -5,22 +5,27 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void
-    {
-        Schema::create('phieu_nhap', function (Blueprint $table) {
-            $table->id('ma_phieu_nhap');
+    public function up()
+{
+    Schema::create('phieu_nhap', function (Blueprint $table) {
+        $table->id('ma_phieu_nhap');
 
-            $table->dateTime('ngay_nhap');
-            $table->unsignedBigInteger('ma_nhan_vien');
+        $table->dateTime('ngay_nhap')->nullable();
+        $table->unsignedBigInteger('ma_nhan_vien');
 
-            $table->foreign('ma_nhan_vien')
-                  ->references('ma_nhan_vien')
-                  ->on('nhan_vien');
-        });
-    }
+        $table->string('ten_nha_cung_cap', 100)->nullable();
+        $table->string('so_dien_thoai_ncc', 15)->nullable();
+        $table->string('email_ncc', 100)->nullable();
+        $table->text('dia_chi_ncc')->nullable();
 
-    public function down(): void
-    {
-        Schema::dropIfExists('phieu_nhap');
-    }
-};
+        $table->foreign('ma_nhan_vien')
+              ->references('ma_nhan_vien')
+              ->on('nhan_vien')
+              ->onDelete('cascade');
+    });
+}
+
+public function down()
+{
+    Schema::dropIfExists('phieu_nhap');
+}};
